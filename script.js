c55b5e1d8b95dd9135b80fa175be1516e0c2d82c
Jose Meneses <146295034+Jmeneses6@users.noreply.github.com>
2023-10-20 22:37:08 -0500

    Reto 1

---
+++ script.js
@@ -0,0 +1,16 @@
+unction calcularTotal() {
+    const cuenta = parseFloat(document.getElementById("cuenta").value);
+    var porcentaje = parseFloat(document.getElementById("porcentaje").value);
+
+    if (isNaN(cuenta) || isNaN(porcentaje)) {
+        alert("ingrese números válidos.");
+        return;
+    }
+
+    var porcentaje = (cuenta * porcentaje) / 100;
+    var totalAmount = cuenta + porcentaje;
+
+    document.getElementById("totalCuenta").innerHTML = "Total de la cuenta: $" + cuenta;
+    document.getElementById("propina").innerHTML = "Propina: $" + porcentaje;
+    document.getElementById("total").innerHTML = "Total: $" + totalAmount;
+}
